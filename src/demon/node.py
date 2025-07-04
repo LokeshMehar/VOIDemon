6ca0c9b8cda7ebe8fff2637c3915901f119fca7e
LokeshMehar <meharlokesh395@gmail.com>
2025-07-04 23:00:00 +0530

    did the node instance single mmodule all

--- src/demon/node.py
+++ src/demon/node.py
@@ -1,11 +1,14 @@
-import requests
-import logging
-import psutil
+import os
+import random
 import time
+import psutil
+import requests
+from singleton import Singleton
+import logging
 import hashlib
 import json
-
-from singleton import Singleton
+import math
+import logging
 logger = logging.getLogger("demon.metrics")
 
 # Define priority levels and update frequencies
@@ -40,7 +43,6 @@ def mk_digest(to_digest):
     hash_object.update(nested_dict_str.encode('utf-8'))
     digest = hash_object.hexdigest()
     return digest
-
 
 def should_send_metric(node, metric, value):
     # Initialize tracking dictionaries if needed
@@ -172,8 +174,7 @@ class Node:
         self.push_mode = None
         self.is_send_data_back = None
         self.metric_last_sent = {}  # Track when each metric was last sent
-        
-        
+
     def start_gossip_counter(self):
         while self.is_alive:
             self.gossip_counter += 1
@@ -191,7 +192,7 @@ class Node:
             self.cycle += 1
             self.transmit(target_count)
             time.sleep(gossip_rate)
-            
+
     def prepare_metadata_and_own_fresh_data(self, time_key):
         metadata = {}
         own_key = self.ip + ':' + self.port
@@ -220,8 +221,7 @@ class Node:
 
         for n in random_nodes:
             self.send_to_node(n, new_time_key)
-            
-            
+
     def update_failure_data(self, new_time_key, n):
         if self.ip + ':' + self.port not in self.data[new_time_key].get(n["ip"] + ':' + n["port"], {}).get("hbState",
                                                                                                            {}).get(
@@ -234,18 +234,64 @@ class Node:
                 self.delete_node_from_nodelist(n["ip"] + ':' + n["port"])
                 self.data[new_time_key][n["ip"] + ':' + n["port"]]["hbState"]["nodeAlive"] = False
         pass
-    
+
     def delete_node_from_nodelist(self, key_to_delete):
         self.node_list.pop(key_to_delete)
-        
+
     def prepare_requested_data(self, time_key, requested_keys):
         requested_data = {}
         for key in requested_keys:
             requested_data[key] = self.data[time_key][key]
         return requested_data
-    
-    
-        
+
+    def reset_failure_data(self, new_time_key, ip_key):
+        if ip_key in self.data[new_time_key]:
+            self.data[new_time_key][ip_key]["hbState"]["failureCount"] = 0
+            self.data[new_time_key][ip_key]["hbState"]["nodeAlive"] = True
+            self.data[new_time_key][ip_key]["hbState"]["failureList"] = []
+        else:
+            self.data[new_time_key].setdefault(ip_key, {}).setdefault("hbState", {})[
+                "failureCount"] = 0
+            self.data[new_time_key].setdefault(ip_key, {}).setdefault("hbState", {})[
+                "failureList"] = []
+            self.data[new_time_key][ip_key]["hbState"]["nodeAlive"] = True
+
+    def update_own_data(self, updates, new_time_key):
+        for u_key in updates:
+            self.data_flow_per_round.setdefault(self.cycle, {})
+            if u_key in self.data[new_time_key]:
+                self.data_flow_per_round[self.cycle].setdefault('fd', 0)
+                self.data_flow_per_round[self.cycle]['fd'] += 1
+            else:
+                self.data_flow_per_round[self.cycle].setdefault('nd', 0)
+                self.data_flow_per_round[self.cycle].setdefault('fd', 0)
+                self.data_flow_per_round[self.cycle]['nd'] += 1
+                self.data_flow_per_round[self.cycle]['fd'] += 1
+            self.data[new_time_key][u_key] = updates[u_key]
+
+        pass
+
+    def send_to_node(self, n, new_time_key):
+        data = self.prepare_metadata_and_own_fresh_data(new_time_key)
+        try:
+            r_metadata_and_updated = requests.post(
+                'http://' + n["ip"] + ':' + '5000' + '/receive_metadata',
+                json=data)
+
+            requested_keys = r_metadata_and_updated.json()['requested_keys']
+            requested_data = self.prepare_requested_data(new_time_key, requested_keys)
+            response = requests.get(
+                'http://' + n["ip"] + ':' + '5000' + '/receive_message?inc_round={}'.format(self.cycle),
+                json=requested_data)
+            self.update_own_data(r_metadata_and_updated.json()['updates'], new_time_key)
+            if response.status_code == 500:
+                self.update_failure_data(new_time_key, n)
+            else:
+                self.reset_failure_data(new_time_key, n["ip"] + ':' + n["port"])
+        except Exception as e:
+            # TODO: in production also add the above failure logic here.
+            logging.error("Error while sending message to node {}: {}".format(n, e))
+
     def set_params(self, ip, port, cycle, node_list, data, is_alive, gossip_counter, failure_counter,
                    monitoring_address, database_address, is_send_data_back, client_thread, counter_thread, data_flow_per_round, push_mode, client_port):
         self.ip = ip
@@ -265,9 +311,56 @@ class Node:
         self.push_mode = push_mode
         self.client_port = client_port
 
-    
+    def get_random_nodes(self, node_list, target_count):
+        new_node_list = []
+        for node in node_list:
+            if self.ip == node['ip']:
+                continue
+            new_node_list.append(node)
+        random_os_data = os.urandom(16)
+        seed = int.from_bytes(random_os_data, byteorder="big")
+        random.seed(seed)
+        return random.sample(new_node_list, target_count)
 
-    
+    def push_latest_data_and_delete_after_push(self):
+        if self.data:
+            latest_time_key = max(self.data.keys())
+            latest_data = self.data[latest_time_key]
+            to_send = self.data
+            self.data = {latest_time_key: latest_data}
+            to_push = {k: v for k, v in to_send.items() if k != latest_time_key}
+            self.session_to_monitoring.post(
+                'http://{}:{}/push_data_to_database?ip={}&port={}&round={}'.format(self.monitoring_address,self.client_port ,self.ip,
+                                                                                 self.port,
+                                                                                 self.cycle), json=to_push)
 
-    
+    def get_filtered_data_by_priority(self, full_data):
+        """Filter metrics based on priority and round number"""
+        if not hasattr(self, 'metric_last_sent'):
+            self.metric_last_sent = {}
+        
+        filtered_data = full_data.copy()
+        
+        # Don't filter if it's the first time sending data
+        if self.cycle <= 1:
+            for metric in METRIC_PRIORITIES:
+                self.metric_last_sent[metric] = self.cycle
+            return filtered_data
+        
+        # Filter app state metrics based on priority
+        if "appSate" in filtered_data:
+            app_state = filtered_data["appSate"].copy()
+            for metric, priority in METRIC_PRIORITIES.items():
+                last_sent = self.metric_last_sent.get(metric, 0)
+                if (self.cycle - last_sent) < priority:
+                    # Remove metrics that don't need to be sent this round
+                    if metric in app_state:
+                        app_state[metric] = "not_updated"
+                else:
+                    # Update last sent time for metrics being sent
+                    self.metric_last_sent[metric] = self.cycle
+            
+            filtered_data["appSate"] = app_state
+        
+        return filtered_data
 
