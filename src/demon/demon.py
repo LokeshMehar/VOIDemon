9369299c528f3db12eb4e2c69e7a4992fd4268b4
LokeshMehar <meharlokesh395@gmail.com>
2025-07-16 23:52:06 +0530

    made a new  route

--- src/demon/demon.py
+++ src/demon/demon.py
@@ -6,6 +6,21 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %
 gossip = Flask(__name__)
 
 
+
+@gossip.route('/metadata', methods=['GET'])
+def get_metadata():
+    if not Node.instance().is_alive:
+        # reset_node()
+        return "Dead Node", 500
+    node = Node.instance()
+    latest_entry = max(node.data.keys(), key=int)
+    metadata = {}
+    for key in node.data[latest_entry]:
+        if 'counter' in node.data[latest_entry][key]:
+            metadata[key] = {'counter': node.data[latest_entry][key]['counter'],
+                             'digest': node.data[latest_entry][key]['digest']}
+    return json.dumps(metadata)
+
 @gossip.route('/VOIDemon', methods=['GET'])
 def get_hello_from_node():
     return "Hello from VOIDemon😈"
