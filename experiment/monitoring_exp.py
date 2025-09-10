76514f5bb22c41bd24deae9d6e766ae57aae8100
LokeshMehar <meharlokesh395@gmail.com>
2025-09-10 23:51:16 +0530

    made db confi and expanded it

--- experiment/monitoring_exp.py
+++ experiment/monitoring_exp.py
@@ -27,28 +27,50 @@ except Exception as e:
 experiment = None
 
 class Run:
-    def __init__(self, node_count, gossip_rate, target_count, run):
+    def __init__(self, node_count, gossip_rate, target_count, run, node_list=None):
         self.db_id = -1
+        self.data_entries_per_ip = {}
+        self.node_list = node_list or []
         self.node_count = node_count
+        self.convergence_round = -1
+        self.convergence_message_count = -1
+        self.message_count = 0
+        self.start_time = None
+        self.convergence_time = None
+        self.is_converged = False
         self.gossip_rate = gossip_rate
         self.target_count = target_count
         self.run = run
-        self.node_list = []
-        self.start_time = None
-        self.is_converged = False
+
+    def set_db_id(self, param):
+        self.db_id = param
 
 class Experiment:
-    def __init__(self, node_count_range, gossip_rate_range, target_count_range, run_count):
+    def __init__(self, node_count_range, gossip_rate_range, target_count_range, run_count, monitoring_address_ip):
         self.db_id = -1
         self.node_count_range = node_count_range
         self.gossip_rate_range = gossip_rate_range
         self.target_count_range = target_count_range
         self.run_count = run_count
         self.runs = []
+        self.monitoring_address_ip = monitoring_address_ip
+        self.db = dbConnector.DemonDB()
+
+    def set_db_id(self, param):
+        self.db_id = param
+
+def get_free_port():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('', 0))
+    port = s.getsockname()[1]
+    s.close()
+    return port
 
 @monitoring_demon.route('/start', methods=['GET'])
 def start_demon():
-    return "OK - Basic structure ready"
+    server_ip = socket.gethostbyname(socket.gethostname())
+    print("Server IP: {}".format(server_ip))
+    return "OK - Database connection ready"
 
 if __name__ == "__main__":
     monitoring_demon.run(host='0.0.0.0', port=4000, debug=False, threaded=True)
